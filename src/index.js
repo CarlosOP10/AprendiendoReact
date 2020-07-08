90ccc013a8d1cd4cc42ebae2297d50af496b67d6
Carlos <carlos@MacBook-Pro-de-Carlos.local>
2020-07-07 22:20:29 -0400

    Ejercicio 3 del tutorial resuelto
    Se uso una lista de listas constante y se recorrio la misma para renderizar los squares

--- src/index.js
+++ src/index.js
@@ -2,21 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// class Square extends React.Component {
-//     // constructor(props) {
-//     //     super(props);
-//     //     this.state = {
-//     //         value: null,
-//     //     };
-//     // }
-//     render() {
-//       return (
-//         <button className="square" onClick={()=> { this.props.onClick()}}>
-//           {this.props.value}
-//         </button>
-//       );
-//     }
-//   }
   function Square(props){
       return (
           <button className="square" onClick={props.onClick}>
@@ -25,21 +10,21 @@ import './index.css';
       );
   }
   class Board extends React.Component {
-    // constructor(props){
-    //     super(props)
-    //     this.state={
-    //         squares: Array(9).fill(null),
-    //         xIsNext: true,
-    //     };
-    // }  
     renderSquare(i) {
       return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)}/>;
     }
    
     render() {
+        const valueSquares=[
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+        ];
+
       return (
         <div>
-          <div className="board-row">
+          {
+          /* <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}      
             {this.renderSquare(2)}
@@ -53,7 +38,17 @@ import './index.css';
             {this.renderSquare(6)}
             {this.renderSquare(7)}
             {this.renderSquare(8)}
-          </div>
+          </div> */
+          valueSquares.map((value,index) => {
+            return <div className="board-row" key={index}>
+                    {
+                        value.map((valor,posicion) => {
+                            return <b key={posicion}>{this.renderSquare(valor)}</b>
+                        })
+                    }
+                   </div>
+           })
+          }
         </div>
       );
     }
